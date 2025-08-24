df2ad8eb62e44714ad51fc6f244728dfa0b3b53f
luuxe <luxelily@gmail.com>
2025-08-24 12:22:38 -0500

    create uploads dir if its not there duh

--- src/app/api/images/upload/route.ts
+++ src/app/api/images/upload/route.ts
@@ -1,36 +1,46 @@
 import { UploadResponse } from "@/app/types/api";
 import { NextResponse } from "next/server";
 import { writeFile } from "fs/promises";
+import fs from "fs";
 import path from "path";
 
 // export async function POST(req: Request) {
-//     // TODO: upload file to storage, disk for now 
+//     // TODO: upload file to storage, disk for now
 //     return NextResponse.json<UploadResponse>({ success: false }, { status: 400 });
 // }
 
 export async function POST(req: Request) {
-    const formData = await req.formData();
-    const file = formData.get("image") as File | null;
+  const formData = await req.formData();
+  const file = formData.get("image") as File | null;
 
-    if (!file) {
-        return NextResponse.json<UploadResponse>({ success: false }, { status: 400 });
+  if (!file) {
+    return NextResponse.json<UploadResponse>(
+      { success: false },
+      { status: 400 }
+    );
+  }
+
+  const arrayBuffer = await file.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+
+  const uploadDir = path.join(process.cwd(), "public", "uploads");
+
+  try {
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir, { recursive: true });
     }
+    await writeFile(path.join(uploadDir, file.name), buffer);
+  } catch (error) {
+    console.error("Error writing file:", error);
+    return NextResponse.json<UploadResponse>(
+      { success: false },
+      { status: 500 }
+    );
+  }
 
-    const arrayBuffer = await file.arrayBuffer();
-    const buffer = Buffer.from(arrayBuffer);
+  const fileUrl = `/uploads/${file.name}`;
 
-    const uploadDir = path.join(process.cwd(), "public", "uploads");
+  console.log("file url", fileUrl);
 
-    try {
-        await writeFile(path.join(uploadDir, file.name), buffer);
-    } catch (error) {
-        console.error("Error writing file:", error);
-        return NextResponse.json<UploadResponse>({ success: false }, { status: 500 });
-    }
-
-    const fileUrl = `/uploads/${file.name}`;
-
-    console.log("file url", fileUrl);
-
-    return NextResponse.json<UploadResponse>({ success: true, url: fileUrl });
+  return NextResponse.json<UploadResponse>({ success: true, url: fileUrl });
 }
